3cc0c3e54bb10f3139184198c99265a7d68bbc30
krbrennan <krb.brennan@gmail.com>
2018-06-14 18:47:27 -0400

    Transitioned from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_14_203518) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "subs", force: :cascade do |t|
     t.integer "moderator_id", null: false
     t.string "name", null: false
